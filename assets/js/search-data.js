b0d8d87b51d6870449f83f6e70d52f4791472501
DanielSprockett <DanielSprockett@users.noreply.github.com>
2025-12-02 03:19:56 +0000

    Deploying to gh-pages from @ SprockettLab/SprockettLab.github.io@d998eca52054b01021752b901d473ceee5ff8bef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-microbiome-assembly-in-early-life",
+          title: 'Microbiome Assembly in Early Life',
+          description: "Where do our microbiomes come from?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
